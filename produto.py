293add4545ae559be1445518849875ba7dcbc458
ThiagoViana07 <thiago_viana99@hotmail.com>
2025-11-26 08:45:08 -0300

    feat:versão inicial do projeto loja-produtos

---
+++ produto.py
@@ -0,0 +1,19 @@
+class Produto:
+    def __init__(self, nome:str, preco:float, estoque:int = 0):
+        self.nome = nome
+        self.preco = preco
+        self.estoque = estoque
+    
+    def __str__(self):
+        return f"{self.nome} - R$ {self.preco:.2f} - Estoque: {self.estoque}"
+
+    def repor(self, qunatidade:int):
+        if quantidade > 0:
+            self.estoque += quantidade
+        
+    def vender(self, quantidade:int) -> bool:
+        if 0 < quantidade <= self.estoque:
+            self.estoque -= quantidade
+            return True
+        return False
+    
